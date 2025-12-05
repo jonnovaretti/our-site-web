04a555e9710bc34647026ec3cc374291014fce40
Jonathan Novaretti <jonn.novaretti@gmail.com>
2025-12-05 06:34:03 -0300

    Adjust layout to give navbar full-height menu area

---
+++ src/views/dashboard.view.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DashboardView() {
+  return <div>dashboard.view.tsx</div>;
+}
+
+export default DashboardView;
---
+++ src/views/new-project.view.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NewProjectView() {
+  return <div>new-project.view.tsx</div>;
+}
+
+export default NewProjectView;
